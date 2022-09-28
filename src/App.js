48a6d992694e019f4f37f9444ac59419b1b308c7
salmamouri <mouriclub@gmail.com>
2022-09-29 03:13:56 +0600

    header & toast added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import Activities from "./Components/Activities/Activities";
 import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Activities></Activities>
       <Blogs></Blogs>
+      <ToastContainer position="top-center"></ToastContainer>
     </div>
   );
 }
--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { toast } from "react-toastify";
 import "./Excercise.css";
 
 const Exercise = ({ exTime, breaktime }) => {
@@ -10,6 +11,12 @@ const Exercise = ({ exTime, breaktime }) => {
       <div className="break-time">
         <h4>Break Time : {breaktime}s </h4>
       </div>
+      <button
+        onClick={() => toast.success("Congratulations!!! You are Done.")}
+        className="complete-btn"
+      >
+        Activity Completed
+      </button>
     </div>
   );
 };
